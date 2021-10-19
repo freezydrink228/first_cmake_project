37ee3667840c4de15bd1cbd806095cef1c70f481
Andrey Kozyakov <student.kozan@gmail.com>
2021-10-19 17:45:22 +0300

    new commit

---
+++ foo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void hello_world()
+{
+	std::cout << "Good morning, My friends!" << std::endl;
+	std::cout << "VALUE is : " << VALUE1 << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "foo.h"
+
+
+
+int main(int argc, char** argv)
+{
+
+
+if (VALUE1 == 50){
+
+	hello_world();
+}else{
+	hello_world();
+	hello_world();
+}
+	return 0;
+}
